c0ca896d2792eb1763ae3e4778f3ccc31bf08c71
Subham95-git <roysubham505@gmail.com>
2025-01-02 23:27:25 +0530

    Array Problems

--- src/com/demo/array/LinearSearchAlgorithm/Demo.java
+++ src/com/demo/array/LinearSearchAlgorithm/Demo.java
@@ -20,3 +20,10 @@ public class Demo {
 
     }
 }
+/*
+Time Complexity:
+
+Best Case: In the best case, the key might be present at the first index. So the best case complexity is O(1)
+Worst Case: In the worst case, the key might be present at the last index i.e., opposite to the end from which the search has started in the list. So the worst-case complexity is O(N) where N is the size of the list.
+Average Case: O(N)
+Auxiliary Space: O(1) as except for the variable to iterate through the list, no other variable is used. */
